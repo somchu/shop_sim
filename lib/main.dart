bd6f6b37eedd62976500bd8ac64b55d0eab787ad
nimtood <somchu29@gmail.com>
2025-05-28 23:59:59 +0700

    20280528_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pastel_theme_package/pastel_theme_ui_package.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,9 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Center(child: Text('ทดสอบ', style: AppTextStyles.body)),
       ),
     );
   }
